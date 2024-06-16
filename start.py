cafcbb73d76302541bd0bf0b7ab304c618e08597
Mohamed Khalaf <mjkhalaf13@gmail.com>
2024-06-16 09:40:18 +0300

    lists using loops

--- start.py
+++ start.py
@@ -1,6 +1,15 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
+a = []
+counter = 1
+for i in range(4):
+    j = 0
+    b = []
+    while j < 3:
+        b.append(counter)
+        counter += 1
+        j += 1
+    a.append(b)
+
+print(a)
+
+
+    
