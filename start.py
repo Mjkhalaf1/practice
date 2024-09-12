9ca288e38c4d547a4a4a34e64bd85d56f9e02a64
Mohamed Khalaf <mjkhalaf13@gmail.com>
2024-09-12 20:28:24 +0300

    various recursive algorithms

--- start.py
+++ start.py
@@ -1,53 +1,74 @@
-def BFS(graph , node):
-    visited = []
-    queue = []
-    queue.append(node)
-    while queue:
-        current = queue[0]
-        for i in graph[current]:
-            if i not in visited:
-                queue.append(i)
-        if current not in visited:
-            print(current ,end=" ")
-            visited.append(queue.pop(0))
+lst1  = [2,6,3,5,1,10,1,0,50]
+tempLst = [0] * (max(lst1)+1)
+for i in lst1:
+    tempLst[i] +=1
+for i in range(1,len(tempLst)):
+    tempLst[i] += tempLst[i-1]
+sortList = [0] * len(lst1)
+for i in reversed(lst1):
+    sortedPosition = tempLst[i] - 1
+    sortList[sortedPosition] = i
+    tempLst[i] -= 1
+print(sortList)
+
+lst1  = [2,6,3,5,1,10,1,50,0]
+for i in range(1,len(lst1)):
+    j = i
+    while(lst1[j] < lst1[j-1] and j >= 1):
+        lst1[j] , lst1[j-1] = lst1[j-1] , lst1[j]
+        j -= 1
+print(lst1)
+
+def factorial(n):
+    if (n==1 or n==0):
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(3))
+
+def palindrome(word, i, j):
+    if i > j:
+        return True
+    else:
+        if(word[i] != word[j]):
+            return False
         else:
-            queue.pop(0)
-            
-def DFS(graph , node):
-    visited = []
-    stack = []
-    stack.append(node)
-    while stack:
-        current = stack.pop()
-        if current not in visited:
-            visited.append(current)
-            print(current, end=" ")
-        for i in reversed(graph[current]):
-            if i not in visited:
-                stack.append(i)
+            return palindrome(word , i+1 , j-1)
+print(palindrome('racecar' , 0 , 6))
 
-graph2 = {
-    '47': ['21', '76'],
-    '21': ['18' , '27'],
-    '76': ['27' , '52' , '82'],
-    '18': [],
-    '27': ['1'],
-    '52': [],
-    '82': [],
-    '1' : []
-}
-BFS(graph2,'47')
-print("----------------------------------")
+def power(x,n):
+    if n==0:
+        return 1
+    else:
+        return x * power(x,n-1)
+print(power(10,3))
 
-graph3 = {
-    'A': ['B', 'G'],
-    'B': ['C', 'D', 'E'],
-    'C': [],
-    'D': [],
-    'E': ['F'],
-    'F': [],
-    'G': ['H'],
-    'H': ['I'],
-    'I': []
-}
-DFS(graph3,'A')
+def sum(lst , i ,j):
+    if(i==j):
+        return lst[i]
+    else:
+        return lst[i] + sum(lst,i+1,j)
+print(sum([10,25,35,40,15,15] , 0 , 5))
+
+def bin(lst, i, j, v):
+    if i > j:
+        return -1  
+    mid = int((i + j) / 2)
+    if lst[mid] == v:
+        return mid
+    elif lst[mid] > v:
+        return bin(lst, i, mid - 1, v)
+    else:
+        return bin(lst, mid + 1, j, v)
+
+print(bin([1, 2, 3, 4, 5], 0, 4, 5))
+print("for odd numbers")
+def oddSum(n):
+    if n == 1:
+        return 1
+    else:
+        if n % 2 == 1:
+            return n + oddSum(n-1)
+        else:
+            return oddSum(n-1)
+print(oddSum(9))
